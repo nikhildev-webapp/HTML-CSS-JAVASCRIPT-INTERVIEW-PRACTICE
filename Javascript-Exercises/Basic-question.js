534bb7da152ed3fd9f4b18a7da07bfdf3e4ee5af
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-05-27 16:21:45 +0530

    javacript exercise-basics

---
+++ Javascript-Exercises/Basic-question.js
@@ -0,0 +1,13 @@
+// javascript basic question
+
+//Question 1-create a variable and store your name and age and print it.
+let Name = 'Nikhil';
+let Age = 21;
+console.log(Name);
+console.log(Age);
+
+//Question2-add tow number without using functions
+let Num1 = 20;
+let Num2 = 30;
+let sumOfNum = Num1 + Num2;
+console.log(sumOfNum);
