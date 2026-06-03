47bde02a207aa4d3f5f3f47ba2ffa6d0926b2ce6
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-06-03 16:31:11 +0530

    intro to typescript it is important in 2026

---
+++ TypeScript-Exercise/introTs.js
@@ -0,0 +1,7 @@
+"use strict";
+let Name = 'Nikhil';
+let Age = 24;
+let isStudent = true;
+console.log(Name);
+console.log(Age);
+console.log(isStudent);
---
+++ TypeScript-Exercise/introTs.ts
@@ -0,0 +1,6 @@
+let Name: string = 'Nikhil';
+let Age: number = 24;
+let isStudent: boolean = true;
+console.log(Name);
+console.log(Age);
+console.log(isStudent);
