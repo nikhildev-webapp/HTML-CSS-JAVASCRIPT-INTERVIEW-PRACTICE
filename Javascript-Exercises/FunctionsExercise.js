ec6957e97ba9e726429381c5632cce3a5e6634ff
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-06-26 19:00:41 +0530

    function exercise

---
+++ Javascript-Exercises/FunctionsExercise.js
@@ -0,0 +1,17 @@
+//Function Exercise
+//Exercise 1-Create addition Function
+function add(num1, num2) {
+    return num1+num2
+}
+console.log(add(22, 33))
+
+//Exercise2-Function to check odd or even
+function CheckNumber(n) {
+    if (n % 2 === 0) {
+        console.log(`The ${n} is Even`)
+    } else {
+        console.log(`The ${n} is Odd`)
+    }
+    
+}
+console.log(CheckNumber(2))
