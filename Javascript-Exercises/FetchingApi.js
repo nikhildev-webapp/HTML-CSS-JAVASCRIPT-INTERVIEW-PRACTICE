12f5daf73edd39ad0409f0d223576069144a10e7
Nikhil Kholiya <nikhilkholiya08@gmail.com>
2026-08-01 16:56:00 +0530

    fetching data from mock api

--- Javascript-Exercises/FetchingApi.js
+++ Javascript-Exercises/FetchingApi.js
@@ -1,2 +1,19 @@
 // Fetchig data from public api using async/await
-console.log('Fetching the Data from Mock Api');
+console.log('Exercise:'+''+'Fetching the Data from Mock Api');
+
+async function fetchUser() {
+    try {
+        console.log('Downlading start...')
+        let response = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status}`); 
+        }
+        let data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log('Error Message:',error)
+    }
+    console.log('Downloading Completed')
+}
+
+fetchUser()
